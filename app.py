19adf0a9cf1c6ed7ed98caba3bf8cccc369319b8
âBruke200IQLulu <ludvigmegeberg@gmail.com>
2026-03-06 10:15:36 +0000

    jeg har linket alle sidene sammen sånn at man kan gå til alle sider fra alle sidene

--- app.py
+++ app.py
@@ -4,11 +4,23 @@ import mysql.connector
 
 
 app = Flask(__name__)
-app.secret_key = "Monstererdigg"
+app.secret_key = "Monster_er_digg"
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
+
+@app.route("/velkommen")
+def velkommen():
+    return render_template("velkommen.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def registrer():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
