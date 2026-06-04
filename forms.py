2fb0a6e32c8d84dba539915888e978316ddbed76
âBruke200IQLulu <ludvigmegeberg@gmail.com>
2026-06-04 08:42:43 +0000

    jeg har lagt til formen som gir admin mulig heten til å legge til nye monstere

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired
 
 class RegisterForm(FlaskForm):
@@ -12,3 +12,8 @@ class LoginForm(FlaskForm):
     username = StringField("Brukernavn", validators=[InputRequired()])
     password = PasswordField("Passord", validators=[InputRequired()])
     submit = SubmitField("Logg inn")
+
+class MonsterForm(FlaskForm):
+    name = StringField("Monster-smak", validators=[InputRequired()])
+    info = TextAreaField("Info")
+    submit = SubmitField("Legg til monster")
